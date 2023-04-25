bb8a253d28f020d42b2a84ec925eee17b904d025
MichalCzajkowski <michcza351@student.polsl.pl>
2023-04-25 16:19:55 +0200

    Jenkinsfile dodany

--- src/test/java/jenkins2/zadanie/ZadanieApplicationTests.java
+++ src/test/java/jenkins2/zadanie/ZadanieApplicationTests.java
@@ -22,7 +22,7 @@ class ZadanieApplicationTests {
 		this.mockMvc.perform(get("/"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().string("Hello Jenkins")); 
+				.andExpect(content().string("Hello Jenkins"));
 	}
 
 }
